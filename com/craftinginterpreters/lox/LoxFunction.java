4a20b4e64f10fdc895d41ac77322de056c8d95de
northbytruth <aranwebster@hotmail.com>
2024-04-16 16:14:52 +0100

    final changes and add lox examples

--- com/craftinginterpreters/lox/LoxFunction.java
+++ com/craftinginterpreters/lox/LoxFunction.java
@@ -5,9 +5,21 @@ import java.util.List;
 public class LoxFunction implements LoxCallable {
 
     private final Stmt.Function declaration;
-    LoxFunction(Stmt.Function declaration){
+    private final Environment closure;
+    private boolean isInitializer;
+
+    LoxFunction(Stmt.Function declaration, Environment closure, boolean isInitializer){
+        this.isInitializer = isInitializer;
         this.declaration = declaration;
+        this.closure = closure;
     }
+
+    LoxFunction bind(LoxInstance instance){
+        Environment environment = new Environment(closure);
+        environment.define("this", instance);
+        return new LoxFunction(declaration, environment, isInitializer);
+    }
+
     @Override
     public int arity() {
         return declaration.params.size();
@@ -15,7 +27,7 @@ public class LoxFunction implements LoxCallable {
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for(int i = 0; i < declaration.params.size(); i++){
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
@@ -23,8 +35,11 @@ public class LoxFunction implements LoxCallable {
         try {
             interpreter.executeBlock(declaration.body, environment);
         } catch (Return returnValue){
+            if(isInitializer) return closure.getAt(0,"this");
             return returnValue.value;
         }
+
+        if(isInitializer) return closure.getAt(0, "this");
         return null;
     }
 
